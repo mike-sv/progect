63790ff6ff6a2a5cd636bfb0fa93ef0f247b53e9
MikeSV <mike.sutulov@gmail.com>
2013-03-29 17:14:28 +0400

    done wits the progect app

---
+++ app/models/micropost.rb
@@ -0,0 +1,7 @@
+class Micropost < ActiveRecord::Base
+  belongs_to :user
+  attr_accessible :contents, :user_id
+  validates :contents, :length => { :maximum => 140 }
+  
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Progect::Application.config.secret_token = '081874c91f3321ffecf036a50352f5c408253a421379e2899c6877877aaa89ded7c8fdbecdf0dc1f82800b06179262fe3f9154916101d369be488edcdb6e4996'
